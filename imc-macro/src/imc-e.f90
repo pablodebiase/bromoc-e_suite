391a0745a5639cba9d2923d70ab535d73f70865b
Pablo M. De Biase <pablodebiase@gmail.com>
2016-12-07 18:00:40 +0000

    Forgot to add new keyword to namelist

--- imc-macro/src/imc-e.f90
+++ imc-macro/src/imc-e.f90
@@ -176,7 +176,7 @@ real*8,allocatable :: potbak(:,:,:)
 
 integer*1 restyp
 !  input
-namelist /input/ nmks,nmks0,lpot,filrdf,filpot,fout,af,fq,b1x,b1y,b1z,b2x,b2y,b2z,b3x,b3y,b3z,dr,iout,iavfac,iav,iprint,regp,dpotm,rtm,eps,temp,iseed,rpdb,rpdbnm,ldmppot,zeromove,lzm,lelec,lrespot,respotnm,lseppot,lseprdf,wpdb,wpdbnm,lrefcrd,ldmppdb,bforce,lfixpot,potfac,potefac
+namelist /input/ nmks,nmks0,lpot,filrdf,filpot,fout,af,fq,b1x,b1y,b1z,b2x,b2y,b2z,b3x,b3y,b3z,dr,iout,iavfac,iav,iprint,regp,dpotm,rtm,eps,temp,iseed,rpdb,rpdbnm,ldmppot,zeromove,lzm,lelec,lrespot,respotnm,lseppot,lseprdf,wpdb,wpdbnm,lrefcrd,ldmppdb,bforce,lfixpot,potfac,potefac,npseppot
 
 label    = 'IMC-E v4.00'
 datestamp = '13-11-2016'
